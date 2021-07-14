123c1ceda41e9911e45240a892ecd996d67ca0e9
lucasmv2205 <lmartins2205@gmail.com>
2021-07-14 20:02:27 -0300

    create foreign keys

--- src/database/migrations/1626302471554-CreateCompliments.ts
+++ src/database/migrations/1626302471554-CreateCompliments.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from "typeorm";
 
 export class CreateCompliments1626302471554 implements MigrationInterface {
 
@@ -37,9 +37,47 @@ export class CreateCompliments1626302471554 implements MigrationInterface {
                         type: "timestamp",
                         default: "now()",
                     }
-                ]
+                ],
+                foreignKeys: [
+                    {
+                        name: "FKUserSenderCompliments",
+                        referencedTableName: "users",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["user_sender"],
+                        onDelete: "SET NULL",
+                        onUpdate: "SET NULL"
+                    },
+                    {
+                        name: "FKUserReceiverCompliments",
+                        referencedTableName: "users",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["user_receiver"],
+                        onDelete: "SET NULL",
+                        onUpdate: "SET NULL"
+                    },
+                    {
+                        name: "FKTagCompliments",
+                        referencedTableName: "tags",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["tag_id"],
+                        onDelete: "SET NULL",
+                        onUpdate: "SET NULL"
+                    }
+                ],
             })
-        )
+        );
+
+        // await queryRunner.createForeignKey(
+        //     "compliments",
+        //     new TableForeignKey({
+        //         name: "FKUserSenderCompliments",
+        //         referencedTableName: "users",
+        //         referencedColumnNames: ["id"],
+        //         columnNames: ["user_sender"],
+        //         onDelete: "SET NULL",
+        //         onUpdate: "SET NULL"
+        //     })
+        // );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
